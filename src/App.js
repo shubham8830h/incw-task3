94fb73517ccb951e9f77f47e0ad7c0b070fe85be
shubham8830h <shubhamhanamane1111@gmail.com>
2023-06-11 10:01:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [headerData, setHeaderData] = useState(null);
+
+  useEffect(() => {
+    const postNumber = async () => {
+      const url = "https://chimpu.xyz/api/post.php";
+      const phoneNumber = "8830461108";
+      try {
+        const response = await axios.post(url, { phone: phoneNumber });
+        const headers = response.headers;
+        setHeaderData(headers);
+        console.log(headers);
+        console.log(response);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    postNumber();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Header Data</h1>
+      {headerData && (
+        <ul>
+          {Object.entries(headerData).map(([name, value]) => (
+            <li key={name}>
+              <strong>{name}:</strong> {value}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
